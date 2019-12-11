7b421596aea62cff83c19a22ad91363e44b93a65
matiusch <matheus.m.prestes@gmail.com>
2019-12-11 10:46:23 -0300

    type

---
+++ produto.js
@@ -0,0 +1,53 @@
+"use strict";
+exports.__esModule = true;
+var Produto = /** @class */ (function () {
+    function Produto(_co, _no, _de, _pr, _es, _li) {
+        this.codigo = _co;
+        this.nome = _no;
+        this.detalhes = _de;
+        this.preco = _pr;
+        this.estoque = _es;
+        this.linkFoto = _li;
+    }
+    Produto.prototype.getCodigo = function () {
+        return this.codigo;
+    };
+    Produto.prototype.setCodigo = function (_co) {
+        this.codigo = _co;
+    };
+    Produto.prototype.getNome = function () {
+        return this.nome;
+    };
+    Produto.prototype.setNome = function (_no) {
+        this.nome = _no;
+    };
+    Produto.prototype.getDetalhes = function () {
+        return this.detalhes;
+    };
+    Produto.prototype.setDetalhes = function (_de) {
+        this.detalhes = _de;
+    };
+    Produto.prototype.getPreco = function () {
+        return this.preco;
+    };
+    Produto.prototype.setPreco = function (_pr) {
+        this.preco = _pr;
+    };
+    Produto.prototype.getEstoque = function () {
+        return this.estoque;
+    };
+    Produto.prototype.setEstoque = function (_es) {
+        this.estoque = _es;
+    };
+    Produto.prototype.getLinkFoto = function () {
+        return this.linkFoto;
+    };
+    Produto.prototype.setLinkfoto = function (_li) {
+        this.linkFoto = _li;
+    };
+    Produto.prototype.getInfo = function () {
+        console.log("Produto = " + this.codigo + "," + this.nome + "," + this.detalhes + "," + this.preco + "," + this.estoque + "," + this.linkFoto);
+    };
+    return Produto;
+}());
+exports.Produto = Produto;
